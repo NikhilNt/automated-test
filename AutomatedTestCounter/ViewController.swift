7bef4d559c1f85089c98fbc0ddc7fad06478419a
Nikhil Tanappagol <nikhiltrans123@gmail.com>
2017-08-24 15:30:57 -0700

    Initial Commit

---
+++ AutomatedTestCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutomatedTestCounter
+//
+//  Created by Nikhil Tanappagol on 8/24/17.
+//  Copyright © 2017 Nikhil Tanappagol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
